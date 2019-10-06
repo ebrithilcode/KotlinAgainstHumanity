5f83fc3f039da2c97244f6bc7955a71e80d0c31d
kilian <kilian.mandon@gmail.com>
2019-10-06 22:48:41 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    application
+}
+
+group = "com.ebrithil.kah"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+project(":server") {
+    apply(plugin="application")
+    apply(plugin="kotlin")
+}
+
+project(":client") {
+    apply(plugin="kotlin")
+}
+
+project(":launcher") {
+    apply(plugin="kotlin")
+}
